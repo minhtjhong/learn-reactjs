87bade55c8cbb48d890874b653633d4dae418e58
Charlie Nguyen <thong.nobita@gmail.com>
2022-11-01 02:23:22 +0700

    JSX

--- src/App.js
+++ src/App.js
@@ -7,16 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          First code. My name is "Charlie Nguyen" ☺
+          <br /><br />
+          Hello World!!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
